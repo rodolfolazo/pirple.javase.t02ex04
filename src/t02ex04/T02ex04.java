de2664ec173c38ab137f5cda0713900d9904f6d3
Rodolfo Lazo <rodolfo.lazo@outlook.com>
2021-01-26 13:26:26 +0000

    Mejorando la versión inicial considerando todos los casos

--- src/t02ex04/T02ex04.java
+++ src/t02ex04/T02ex04.java
@@ -1,5 +1,5 @@
 /*
- * Floatin : Part B
+ * Floating : Part B
  */
 package t02ex04;
 
@@ -14,22 +14,28 @@ public class T02ex04 {
     public static void main(String[] args) {
         // Primero se realiza la operación y luego se crea y asigna a la variable
         // Podemos forzar una variable a que se comporte como otra con casting
-               
-        int iNum1 = 5/4;
-        float fNum1 = 5/4f;
-        double dNum1 = 5/4;
+           
+        //Probando al integer
+        float fNum1 = 3.1415f;
+        double dNum1 = 3.1415;
+        int iNum1 = (int)fNum1;
+        //int iNum1 = dNum1;
+        System.out.println(iNum1);
         
+        //Probando al float
         int iNum2 = 5/4;
-        float fNum2 = 5/4;
-        double dNum2 = 5/4;
-        
-        int iNum3 = 5/4;
-        float fNum3 = 5/4;
-        double dNum3 = 5/4;
-        iNum3 = (int)fNum3;
-        
-        System.out.println(iNum3);
-        
+        double dNum2 = 3.1415;
+        //float fNum2 =iNum2;
+        //float fNum2 = dNum2;
+        //System.out.println(fNum2);
+                       
+        //Probando al double
+        int iNum3 = 5;
+        float fNum3 = 3.1415f;
+        //double dNum3 = fNum3;
+        //double dNum3 = iNum3;
+        //System.out.println(dNum3); 
+      
     }
     
 }
